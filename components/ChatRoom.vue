6d926c4741d15e67e8905904618014214c112c0c
BrianM0330 <BrianM0330@gmail.com>
2023-06-27 18:50:08 -0500

    more updates

--- components/ChatRoom.vue
+++ components/ChatRoom.vue
@@ -1,11 +1,23 @@
 <template>
-  <div class="messageBox bg-red bg-slate-50 h-1/2 flex w-96 flex-col-reverse gap-2 px-1">
-    <div class="w-96 bg-slate-50 p-1" v-for="(message, index) in messages" :key="index">
+  <div class="flex flex-col-reverse gap-2 px-1 messageBox bg-red bg-white border-2 border-black h-1/2 w-96">
+    
+    <div id="submissionArea" class="flex w-full gap-1">
+      <input id="messageBox" class="w-full h-10 border border-black" v-model="currentMessage" />
+      <div> </div>
+      <div @click="insertMessage" type="submit" class="w-10 h-10 margin-left flex justify-center items-center border border-black">
+        <img src="~/assets/icons/corner-down-right.png" />
+      </div>
+    </div>
+
+    <div id="messages" class="p-1 w-full bg-white" v-for="(message, index) in messages" :key="index">
       {{message }}
     </div>
+
+    <div id="subRow"></div>
+
+    <div id="numberOfPeople" class=""> chat (112) </div>
+
   </div>
-    <input v-model="currentMessage" />
-    <button @click.prevent="insertMessage" type="submit"> Send Message </button>
 </template>
 
 <script setup>
