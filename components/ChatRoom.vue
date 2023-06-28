a55ef0adc9e4dec9edfcd06fb1ad1de060464672
emily <emily.readey@gmail.com>
2023-06-27 22:08:45 -0700

    hover and focus

--- components/ChatRoom.vue
+++ components/ChatRoom.vue
@@ -1,8 +1,10 @@
 <template>
   <div class="font-lores flex flex-col gap-2 p-1 messageBox bg-red bg-white border-2 border-black h-1/2 w-96">
     <div id= "title" class="flex w-96 h-12 -m-1.5 border-2 border-black items-center justify-self end">
-      <div id="numberOfPeople" class="flex w-full p-2 text-xl"> chat ({{ userCount }}) </div>
-      <div id="minimizeIcon" class="h-6 w-6 margin-left mr-2 justify-center items-center">
+      <div id="numberOfPeople" class="flex w-full p-1 text-lg"> chat ({{ userCount }}) </div>
+      <div id="minimizeIcon" class="h-6 w-6 margin-left mr-2 justify-center items-center 
+      hover:bg-black
+      ">
         <img src="~/assets/icons/minus.png" /> 
       </div>
     </div>
@@ -20,10 +22,15 @@
     <form @submit.prevent="$emit('send', message); message = ''" id="submissionArea" class="flex w-full h-10 gap-1">
       <input
         id="messageBox"
-        class="w-full h-10 p-1 border-2 border-black justify-top"
+        class="w-full h-10 p-1 border-2 border-black justify-top
+        hover:border-blockblue
+        focus:border-blockblue focus:outline-none focus:caret-blockblue"
         v-model="message"
       />
-      <button type="submit" class="w-12 h-10 margin-left flex justify-center items-center border-2 border-black">
+      <button type="submit" class="
+      w-12 h-10 margin-left flex justify-center 
+      items-center border-2 border-black
+      hover:border-blockblue">
         <img src="~/assets/icons/corner-down-right.png" />
       </button>
     </form>
